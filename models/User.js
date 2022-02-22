8188a0539621dfd23f859cbdc4e3cd874c565093
Nathaniel Boucher <93409883+natejboucher@users.noreply.github.com>
2022-02-21 20:44:40 -0500

    added password hashin using bcrypt to creating user and updating user

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
 
 // create our User model
 class User extends Model { }
@@ -44,17 +45,25 @@ User.init(
         }
     },
     {
-        // pass in our imported sequelize connection (the direct connection to our database)
+        hooks: {
+            async beforeCreate(newUserData) {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                return newUserData;
+            },
+            // set up beforeUpdate lifecycle "hook" functionality
+            async beforeUpdate(updatedUserData) {
+                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+                return updatedUserData;
+            }
+        },
         sequelize,
-        // don't automatically create createdAt/updatedAt timestamp fields
         timestamps: false,
-        // don't pluralize name of database table
         freezeTableName: true,
-        // use underscores instead of camel-casing (i.e. `comment_text` and not `commentText`)
         underscored: true,
-        // make it so our model name stays lowercase in the database
         modelName: 'user'
-    }
-);
+      }
+    );
 
 module.exports = User;
+
+// set up beforeCreate lifecycle "hook" functionality
