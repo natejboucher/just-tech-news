6effa9d6287e058fd362fb2c92249f4d38296435
Nathaniel Boucher <93409883+natejboucher@users.noreply.github.com>
2022-02-27 13:40:38 -0500

    added comment and post routes for comments. added comment data for user information routes

--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -2,7 +2,13 @@ const router = require('express').Router();
 const { Comment } = require('../../models');
 
 router.get('/', (req, res) => {
-
+    console.log('==============');
+    Comment.findAll()
+    .then(dbCommentData => res.json(dbCommentData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
 });
 
 router.post('/', (req, res) => {
@@ -19,7 +25,22 @@ router.post('/', (req, res) => {
 });
 
 router.delete('/:id', (req, res) => {
-
+    Comment.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+        .then(dbCommentData => {
+            if(!dbCommentData) {
+                res.status(404).json({ message: "No comment found with that ID."});
+                return;
+            }
+            res.json(dbCommentData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
 module.exports = router;
